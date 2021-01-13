8348930af017eacc87c77e43b7deb26f15a95fb6
Jake Luecke <jrluecke95@gmail.com>
2021-01-13 17:11:45 -0500

    adding comments

--- long_vowels.py
+++ long_vowels.py
@@ -2,17 +2,20 @@ string = "good"
 vowels = ["a", "e", "i", "o", "u"]
 empty_string = ""
 
+#for loop that runs through each index in string first
+# then checks to see if each string[index] is in the list vowels
+# after that - setting the letter to be replaced to string[index of letter] helped simplify things in end expression
+# diong the same for the "range" of double vowels - use 2 because +1 doesn't do anything doofus
+# and then setting empty string = result of replacement
+
 for i in range(len(string)):
     if string[i] in vowels:
         if string[i] == string[i+1]:
             letter = string[i]
-            print(letter)
             double = string[i:i+2]
-            print(double)
             empty_string = string.replace(double, letter*5)
 
 print(empty_string)
-
 
 #loop through string
 # if you encouter vowel check if next one is vowel
