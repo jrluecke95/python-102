d190b18e2a73423ca608091cf2752f012db06a50
Jake Luecke <jrluecke95@gmail.com>
2021-01-13 11:59:00 -0500

    further progress on smallest num with user input - cleaned up excess code - still working on getting a full list to print out

--- smallest_num.py
+++ smallest_num.py
@@ -1,11 +1,20 @@
+# getting user input for length of list here
 while True:
     try:
-        length_of_list = int(input("How long do you want your list to be? "))
-        if length_of_list > 0:
+        length = int(input("How long do you want your list to be? "))
+        if length > 0:
             break
         else:
-            
+            False
     except ValueError:
         print("please enter a valid number")
 
-# print(small_num)
+for i in range(length):
+    list_of_nums = [ ]
+    try:
+        num = int(input(f"What number do you want the {i + 1} item in the list to be? "))
+    except ValueError:
+        print("please enter a valid number")
+    list_of_nums.append(num)
+
+print(list_of_nums)
